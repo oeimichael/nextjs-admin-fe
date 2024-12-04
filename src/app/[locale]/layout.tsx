71d1c9d0204ad6fbef09884aaa6f4ff488d1c020
fifi.kumalasari <fifi.kumalasari@sera.astra.co.id>
2024-12-04 21:31:01 +0700

    adding env

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,15 +2,22 @@
 
 import Navbar from '@/components/navbar';
 import Sidebar from '@/components/sidebar';
+import { useEffect, useState } from 'react';
 import '@/styles/global.css';
 
 export default function RootLayout(props: {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-  const url = '/login'
-  const pathArray = url.split('/');
+  const [pathArray, setPathArray] = useState<string[]>([]);
 
+  useEffect(() => {
+    // Code here only runs on the client (browser)
+    if (typeof window !== 'undefined') {
+      const path = window.location.pathname.split('/');
+      setPathArray(path);
+    }
+  }, []); // This effect runs only once, after the component mounts
   if (pathArray[1] === 'login' || pathArray[1] === 'register') {
     return (
       <html lang="en">
